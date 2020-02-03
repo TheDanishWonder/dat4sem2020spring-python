79fb0cab6bca3db2f7f6fc0b859c1f966960672b
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2020-02-03 20:23:29 +0100

    Exercises done

--- my_modules/global_warming.py
+++
@@ -1,4 +0,0 @@
-def global_warming_status():
-    return 'The globe is heating up!'
-status = global_warming_status()
-print(status)
--- my_modules/un.py
+++
@@ -1,5 +0,0 @@
-import global_warming
-
-status = global_warming.global_warming_status()
-print('UN expert panel says: ' + status)
-print(__name__)
